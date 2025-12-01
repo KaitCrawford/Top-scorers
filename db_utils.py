6774f14ab8b627f9273fea8bfbf5a08dd3762fa9
Kaitlyn <kaitlyn@praekelt.org>
2025-12-01 22:52:23 +0200

    Slight refactor of db_utils.py to reduce duplication and update comments

--- db_utils.py
+++ db_utils.py
@@ -6,7 +6,7 @@ class UserScoreBase(SQLModel):
     This is a model to represent the data for user scores. It is not a table in the
     database
     NOTE: We could add a UniqueConstraint across first_name and second_name to prevent
-    duplicate users
+    duplicate users. For now we just handle that when inserting rows
     """
     first_name: str = Field(index=True)
     second_name: str = Field(index=True)
@@ -21,6 +21,7 @@ class UserScore(UserScoreBase, table=True):
 
 # Setup the db connection
 # NOTE: We should handle connection errors here
+# This should probably be in a settings file of some sort
 db_filename = "database.db"
 sqlite_url = f"sqlite:///{db_filename}"
 
@@ -47,7 +48,7 @@ def create_or_update_user_score(new_user_score: UserScoreBase, session: Session)
     Returns the new/updated object
     """
     # NOTE: We take in the session object here so that the calling code can determine
-    # if the session should be closed or reused.
+    # how the session should be managed.
 
     # Check for users with the same names in the database
     sql_expr_obj = select(UserScore).where(
@@ -55,20 +56,17 @@ def create_or_update_user_score(new_user_score: UserScoreBase, session: Session)
         func.lower(UserScore.second_name) == new_user_score.second_name.lower()
     )
     user_scores = session.exec(sql_expr_obj).all()
-    if len(user_scores) == 0:
-        # The user doesn't exist, so create it
-        session.add(new_user_score)
-        session.commit()
-        return new_user_score
+
     if len(user_scores) > 0:
         # The user does exist, so update it
-        # NOTE: We're ignoring duplicates but it would be better to handle them
-        # NOTE: We shouldn't update the row if the score is the same
+        # NOTE: We're ignoring the situation where there's more than 1 row but it would
+        # be better to handle it since they might have been created by something else
+        # NOTE: It's more efficient not to update the row if the score is the same
         # (Depends on context, if we were storing modified times it might makes sense
         # to touch the row even if the score is the same)
         user_scores[0].score = new_user_score.score
         new_user_score = user_scores[0]
-        session.add(new_user_score)
-        session.commit()
-        return new_user_score
+    session.add(new_user_score)
+    session.commit()
+    return new_user_score
 
