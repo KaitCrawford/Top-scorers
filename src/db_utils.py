75dd2b667febda60a8d8ba1bb72266b95f161861
Kaitlyn <kaitlyn@praekelt.org>
2025-12-03 16:55:25 +0200

    Stop echoing all db info

--- src/db_utils.py
+++ src/db_utils.py
@@ -38,7 +38,7 @@ db_filename = "database.db"
 sqlite_url = f"sqlite:///{db_filename}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args, echo=True)
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def create_db_and_tables():
     """
